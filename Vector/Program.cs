160c4efbd5218052566992159154ea65bc1e47ad
AndreyValchuk <123214867a@gmail.com>
2021-03-17 00:39:23 +0300

    Add project files.

---
+++ Vector/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Vector
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                Console.WriteLine("Создаем первый вектор:");
+                Console.Write("Введите значение координаты x: ");
+                double x = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Введите значение координаты y: ");
+                double y = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Введите значение координаты z: ");
+                double z = Convert.ToDouble(Console.ReadLine());
+
+                Vector vectorA = new Vector(x, y, z);
+
+                Console.WriteLine($"Вектор № 1: {vectorA}");
+
+                Console.WriteLine("Создаем второй вектор:");
+                Console.Write("Введите значение координаты x: ");
+                x = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Введите значение координаты y: ");
+                y = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Введите значение координаты z: ");
+                z = Convert.ToDouble(Console.ReadLine());
+
+                Vector vectorB = new Vector(x, y, z);
+
+                Console.WriteLine($"Вектор № 2: {vectorB}");
+
+                Console.WriteLine($"\nРезультат сложения векторов: {vectorA + vectorB}");
+                Console.WriteLine($"Результат вычитания вектора 2 из вектора 1: {vectorA - vectorB}");
+                Console.WriteLine($"Результат векторного произведения: {vectorA * vectorB}");
+            }
+            catch
+            {
+                Console.WriteLine("Что-то пошло не так...");
+            }
+        }
+    }
+}
---
+++ Vector/Vector.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+
+namespace Vector
+{
+    public class Vector : ICloneable
+    {
+        public double X { get; private set; }
+
+        public double Y { get; private set; }
+
+        public double Z { get; private set; }
+
+        public Vector(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public Vector(Vector vector)
+        {
+            X = vector.X;
+            Y = vector.Y;
+            Z = vector.Z;
+        }
+
+        public static Vector operator +(Vector a, Vector b) => new Vector(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+
+        public static Vector operator -(Vector a, Vector b) => new Vector(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
+
+        public static Vector operator *(Vector a, Vector b) 
+            => new Vector(a.Y * b.Z - a.Z * b.Y, a.Z * b.X - a.X * b.Z, a.X * b.Y - a.Y * b.X);
+
+        public override string ToString()
+        {
+            return $"({X.ToString(CultureInfo.InvariantCulture)}, {Y.ToString(CultureInfo.InvariantCulture)}," +
+                $" {Z.ToString(CultureInfo.InvariantCulture)})";
+        }
+
+        public object Clone() => new Vector(this);
+    }
+}
